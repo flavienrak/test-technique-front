7af76d22a8a2c67b4f54ed8a11034f4c98a6dd54
flavienrak <flavien.andrisoarak@gmail.com>
2025-11-08 23:06:24 +0300

    feat(init): inital configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,12 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 import prettierPlugin from 'eslint-plugin-prettier';
 import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default withNuxt(
-  // Your custom configs here
-  {
-    plugins: {
-      prettier: prettierPlugin,
-    },
-    rules: {
-      ...prettierRecommended.rules,
-      'prettier/prettier': 'error',
-    },
+export default withNuxt({
+  plugins: {
+    prettier: prettierPlugin,
   },
-);
+  rules: {
+    ...prettierRecommended.rules,
+    'prettier/prettier': 'error',
+  },
+});
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@pinia/nuxt',
+    [
+      '@nuxtjs/i18n',
+      {
+        locales: [
+          { code: 'fr', language: 'fr-FR', file: 'fr.json' },
+          { code: 'en', language: 'en-US', file: 'en.json' },
+        ],
+        defaultLocale: 'fr',
+        strategy: 'prefix_except_default',
+      },
+    ],
+  ],
 
   devtools: {
     enabled: true,
