b734493703d791adeaa2fa21bbb59b5b1a154726
flavienrak <flavien.andrisoarak@gmail.com>
2025-11-08 22:17:18 +0300

    feat(prettier): config prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,17 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default withNuxt(
   // Your custom configs here
-)
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettierRecommended.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+);
